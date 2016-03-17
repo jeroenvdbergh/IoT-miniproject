fa1e31799d2b846bab539dbed4f89b1190d02760
Jeroen van den Bergh <jeroenvandenbergh1996@hotmail.com>
2016-03-17 19:53:30 +0100

    test

--- Scripts/Custom/main.js
+++ Scripts/Custom/main.js
@@ -24,8 +24,6 @@ $(document).ready(function(){
 
     //console.log(pattern);
 
-    hallo
-
     var data = {
         labels: ["January", "February", "March", "April", "May", "June", "July"],
         datasets: [
