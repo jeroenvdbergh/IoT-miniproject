63b5996953717dd27e3e32082a2c9e397dea7995
jeroenvdbergh <jeroenvandenbergh1996@hotmail.com>
2016-04-11 16:31:06 +0200

    Revert "Update: bug fixes"
    This reverts commit 7a05d7079b6bab781ef46ccae34a394950bce0fa.

--- NodeServer.js
+++ NodeServer.js
@@ -1,39 +1,58 @@
-//Express is required for creating Node.js based web apps
-var express = require('express');
+/**
+ * Created by Jeroen on 25/02/2016.
+ */
+// Load the net module to create a tcp server.
+//var net = require('net');
+//
+//// Creates a new TCP server. The handler argument is automatically set as a listener for the 'connection' event
+//var server = net.createServer(function (socket) {
+//
+//    // Every time someone connects, tell them hello and then close the connection.
+//    console.log("Connection from " + socket.remoteAddress);
+//    socket.end("Hello World\n");
+//
+//});
+//
+//// Fire up the server bound to port 7000 on localhost
+//server.listen(7000, "localhost");
+//
+//// Put a friendly message on the terminal
+//console.log("TCP server listening on port 7000 at localhost.");
 
-var app = express();
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    port = process.argv[2] || 8887;
 
-app.set('port', 3300);
+http.createServer(function(request, response) {
 
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('smartmeter.db');
+    var uri = url.parse(request.url).pathname
+        , filename = path.join(process.cwd(), uri);
 
+    fs.exists(filename, function(exists) {
+        if(!exists) {
+            response.writeHead(404, {"Content-Type": "text/plain"});
+            response.write("404 Not Found\n");
+            response.end();
+            return;
+        }
 
-//Starting up the server on the port: 3300
-app.listen(app.get('port'), function(){
-    console.log('Server up: http://localhost:' + app.get('port'));
-});
-app.use(express.static('public'));
-app.use(express.static('files'));
+        if (fs.statSync(filename).isDirectory()) filename += '/index.html';
 
-app.use("/", express.static(__dirname + '/'));
+        fs.readFile(filename, "binary", function(err, file) {
+            if(err) {
+                response.writeHead(500, {"Content-Type": "text/plain"});
+                response.write(err + "\n");
+                response.end();
+                return;
+            }
 
-app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/index.html');
-});
-
-app.get("/allValues", function(req, res){
-
-    var result = [];
-
-    db.serialize(function () {
-        db.each("SELECT * FROM data", function (err, row) {
-            result.push(row);
-            result.push(err.message);
+            response.writeHead(200);
+            response.write(file, "binary");
+            response.end();
         });
     });
+}).listen(parseInt(port, 10));
 
-    res.json(result);
-
-
-});
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
--- Scripts/Custom/main.js
+++ Scripts/Custom/main.js
@@ -2,200 +2,79 @@
  * Created by Jeroen on 25/02/2016.
  */
 
+$(document).ready(function(){
 
-// Normaal moet de delay op minsten 10000 staan aangezien de database om de 10 seconde geupdate wordt.
-var speed = 10000;
-var numberOfValues;
-var average;
-var allData = [];
-var lastValue;
-
-//Alle gebruikswaardes bij elkaar opgeteld. Deze variable wordt gebruikt voor het berekenen van het gemiddeld gebruik.
-var sum = 0;
-
-
-$(document).ready(function () {
-    $("#huiskamer").attr("src","/Images/lightbulb-on.png");
-    //
-    $("#dialog").dialog();
-
-    //Datum van vandaag
-    var date = new Date();
-
-    //substract 1 month van de datum af. Vanaf deze datum gaan we het gemiddelde verbruik berekenen.
-    date.setDate(date.getDate() - 30);
-
-    date = $.datepicker.formatDate('yy-mm-dd', date);
-
-    var labels = [];
-    var values = [];
-
-    $.ajax({
-
-        url: 'http://192.168.1.34:27017/allValues',
-        type: 'GET',
-        dataType: "json",
-        crossDomain: true,
-        success: function (data) {
-
-            allData = JSON.parse(data);
-
-            $.each(allData, function (index, value) {
-
-                //Hier staat alle code om de format van het data array aan te passen zodat het te gebruiken is in de grafieken
-                //Nergens op andere plekken in de code dit doen!
-                value.huidigverbruik = value.huidigverbruik.slice(10, 17);
-
-                if (value.datum > date) {
-                    sum = sum + parseFloat(value.huidigverbruik);
-                }
-
-            });
-            //afronden
-            average = Math.round(sum / allData.length * 1000)/1000;
-
-        }
-    }).done(function () {
-
-        init();
-
-        var yesterday = new Date(new Date().getTime() - (24 * 60 * 60 * 1000));
-        var time = yesterday.getHours() + ":" + yesterday.getMinutes() + ":" + yesterday.getSeconds();
-        var date24HoursAgo = yesterday.getFullYear() + "-" + ("0" + (yesterday.getMonth() + 1)).slice(-2) + "-" + ("0" + yesterday.getDate()).slice(-2);
-
-        drawChartByDate("#chart-1", date24HoursAgo, "time");
-        drawChartByDate("#chart-2", date, "date");
+    $(function main() {
+        //$("#canvas_id").drawLineChart();
     });
 
-    //getLastValue();
-    setInterval(function () {
-        //getLastValue();
-    }, speed);
+    //var chart = new Chart("temperatuur", ["1", "2"],["25", "36"]);
+    //
+    ////console.log(chart.dates);
+    //
+    //var testarray = [5,2,6,1,5,3,56,2,3,0,2,4,234,1,5,53];
+    //
+    //$.each(testarray, function(index, value){
+    //
+    //   console.log("index: " + index + " value: " + value);
+    //
+    //});
+
+   // var pattern = /aa/;
+
+    //console.log(pattern);
+
+    var data = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    };
+
+    $("#chart-1").drawLineChart(data);
+    $("#chart-2").drawLineChart(data);
+    $("#chart-3").drawLineChart(data);
+    $("#chart-4").drawLineChart(data);
+
+    updateChart1();
+
+
 });
 
-function drawChartByDate(container, date, xAxis) {
-
-    var datum = [];
-    var tijd = [];
-    var verbruik = [];
-
-    $.each(allData, function (index, value) {
-
-        if (value.datum >= date) {
-            datum.push(value.datum);
-            tijd.push(value.tijd);
-            verbruik.push(parseFloat(value.huidigverbruik));
-        }
-    });
-
-   switch (xAxis){
-       case "time":
-           $(container).drawLineChart(tijd, verbruik);
-           break;
-
-       case "date":
-           $(container).drawLineChart(datum, verbruik);
-           break;
-   }
-
-}
-
-function init() {
-    $('#energy-meter-huidig').drawGaugeChart('huidig', 0);
-    $('#energy-meter-gemiddeld').drawGaugeChart('gemiddeld', average);
-
-    angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache']).controller('AppCtrl', function($scope) {
-        $scope.myDate = new Date();
-
-        $scope.minDate = new Date(
-            $scope.myDate.getFullYear(),
-            $scope.myDate.getMonth() - 2,
-            $scope.myDate.getDate());
-
-        $scope.maxDate = new Date(
-            $scope.myDate.getFullYear(),
-            $scope.myDate.getMonth() + 2,
-            $scope.myDate.getDate());
-
-        $scope.onlyWeekendsPredicate = function(date) {
-            var day = date.getDay();
-            return day === 0 || day === 6;
-        }
-    });
+function updateChart1(){
 
 
-    //initialiseer de jQuery dialog
-    $("#dialog").dialog();
-}
-
-function getLastValue() {
-
-    $.ajax({
-
-        url: 'http://192.168.1.34:27017/newValue',
-        type: 'GET',
-        dataType: "json",
-        crossDomain: true,
-        success: function (data) {
-
-            console.log(data);
-
-            //Check of data geen null is, als data wel null is, is er geen nieuwe data gevonden
-            if (data != "null") {
-
-                    if (lastValue != data) {
-                        lastValue = data;
-                    }
-
-                    data = JSON.parse(data);
-
-                    allData.push(data);
-                    //Hier staat de code om de data in de JSON array te bewerken zodat het gebruikt kan worden in de grafieken
-                    data.huidigverbruik = data.huidigverbruik.slice(10, 17);
 
 
-                    //Zoek de huidig gebruik Gauge chart
-                    var huidigChart = getChartReferenceByClassName('huidig');
-                    //Update de waarde van de Gauge chart met de nieuwe gevonden waarde
 
-                    var temp = parseFloat(data.huidigverbruik);
-                    huidigChart.series[0].points[0].update(Math.round(temp*1000)/1000, 10);
+        Chart.instances['chart-0'].addData([Math.random() * 100, Math.random() * 100], "hallo")
+        Chart.instances['chart-0'].removeData();
 
-                    //average = sum / allData.length;
-                    //
-                    //var gemiddeldChart = getChartReferenceByClassName('gemiddeld');
-                    //gemiddeldChart.series[0].points[0].update(parseFloat(average, 10));
-                } else {
-                    $("#dialog").dialog("open");
-                }
-            }
-    });
-}
-
-
-function updateValues() {
-
-    //var huidigChart = getChartReferenceByClassName('huidig');
-    //var gemiddeldChart = getChartReferenceByClassName('gemiddeld');
-
-    //huidigChart.series[0].points[0].update(Math.random() * 100);
-    //gemiddeldChart.series[0].points[0].update(Math.random() * 100);
-
-    //Update Energie verbruik afgelopen 24 uur chart.
-    //Voeg nieuwe waarde toe en verwijder laatste waarde
-    Chart.instances['chart-0'].addData([Math.random() * 100], "hallo");
-    Chart.instances['chart-0'].removeData();
-
-    //Update Energie verbruik afgelopen maand door nieuwe waardes toe te voegen.
-    Chart.instances['chart-1'].addData([Math.random() * 100], "hallo")
-    Chart.instances['chart-1'].removeData();
 
 
 }
 
-function getChartReferenceByClassName(className) {
-
-    var foundChart = $('.' + className + '').eq(0).parent().highcharts();
-
-    return foundChart;
-}
+//function Chart(name, dates, data){
+//    this.name = name;
+//    this.dates = dates;
+//    this.data = data;
+//}
